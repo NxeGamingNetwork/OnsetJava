cec8d4b8e032d2235f9b9220c6707d256fe0f827
JanHolger <jan@bebendorf.eu>
2019-09-30 04:36:26 +0200

    Refactored get_global in lua

--- OnsetJava-Simple/src/main/resources/lua/client/get_global.lua
+++ OnsetJava-Simple/src/main/resources/lua/client/get_global.lua
@@ -1,38 +1,3 @@
 function get_global(unused, name)
-    if _ENV[name] ~= nil then
-        return _ENV[name]
-    end
-    if name == "CreateSound" then
-        return CreateSound
-    end
-    if name == "CreateSound3D" then
-        return CreateSound3D
-    end
-    if name == "CreateWebUI" then
-        return CreateWebUI
-    end
-    if name == "CreateWebUI3D" then
-        return CreateWebUI3D
-    end
-    if name == "DestroySound" then
-        return DestroySound
-    end
-    if name == "DestroyWebUI" then
-        return DestroyWebUI
-    end
-    if name == "ExecuteWebJS" then
-        return ExecuteWebJS
-    end
-    if name == "SetWebURL" then
-        return SetWebURL
-    end
-    if name == "SetWebAlignment" then
-        return SetWebAlignment
-    end
-    if name == "SetWebAnchors" then
-        return SetWebAnchors
-    end
-    if name == "SetWebVisibility" then
-        return SetWebVisibility
-    end
+    return _ENV[name]
 end
