9a8bc125c5276baad63bcb4423eeb32c7d0237f0
JanHolger <jan@bebendorf.eu>
2019-10-01 16:06:24 +0200

    Added a distance method to Vector3i

--- OnsetJava-API/src/main/java/net/onfirenetwork/onsetjava/api/util/Vector3i.java
+++ OnsetJava-API/src/main/java/net/onfirenetwork/onsetjava/api/util/Vector3i.java
@@ -17,4 +17,11 @@ public class Vector3i {
     public Location toLocation() {
         return new Location(x, y, z);
     }
+    public double distance(Vector3i other){
+        return Math.sqrt(
+                Math.pow(Math.max(x, other.x) - Math.min(x, other.x), 2) +
+                Math.pow(Math.max(y, other.y) - Math.min(y, other.y), 2) +
+                Math.pow(Math.max(z, other.z) - Math.min(z, other.z), 2)
+        );
+    }
 }
