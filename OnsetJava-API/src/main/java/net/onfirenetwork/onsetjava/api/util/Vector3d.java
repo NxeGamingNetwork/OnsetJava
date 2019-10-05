07819bc3cb0b99ec5ed4016c7114db56c1da89c9
x7airworker <jake@jakes-co.de>
2019-10-05 22:15:00 +0200

    Removed getDistance

--- OnsetJava-API/src/main/java/net/onfirenetwork/onsetjava/api/util/Vector3d.java
+++ OnsetJava-API/src/main/java/net/onfirenetwork/onsetjava/api/util/Vector3d.java
@@ -14,8 +14,4 @@ public class Vector3d {
     double x;
     double y;
     double z;
-
-    public double getDistance(Vector3d otherVector) {
-        return Math.abs(Math.sqrt(Math.pow(x - otherVector.x, 2) + Math.pow(y - otherVector.y, 2) + Math.pow(z - otherVector.z, 2)));
-    }
 }
